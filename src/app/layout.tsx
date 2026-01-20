7f8df6767563a0e3fc00ed82c4df3b15edf1f6be
Reyan Jimenez <reyanjimenez@gmail.com>
2026-01-19 20:08:01 -0400

    Fix: Fixed favicon display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="es" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
